c383849875892d25cf46a9b9592fd816d4f63f72
byeonsejun <qustpwns93@gmail.com>
2024-04-01 16:20:23 +0900

    find err 1.6

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -5,7 +5,7 @@ import GoogleMapContainer from './GoogleMapContainer';
 export default function Map() {
   return (
     <div className="grow w-full h-full border border-[#ededed]" id="map">
-      <GoogleMapContainer />
+      {/* <GoogleMapContainer /> */}
       {/* <MapChart /> */}
     </div>
   );
