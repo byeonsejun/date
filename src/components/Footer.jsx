5ce0dc52c4eedb96e1e706b12db41ac846ce84f6
byeonsejun <qustpwns93@gmail.com>
2024-04-30 11:45:06 +0900

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,7 @@
 export default function Footer() {
-  return <footer className="h-8 bg-black text-white flex items-center justify-center mt-3">Footer</footer>;
+  return (
+    <footer className="h-8 bg-black text-white flex items-center justify-center mt-3">
+      Copyright ⓒ 2024 BYUNSEJUN.ALL Rights Reserved.
+    </footer>
+  );
 }
